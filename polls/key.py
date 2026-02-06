15eb37c255c982242cd1ff68d0f83a9193d71e6e
nhanhtt1 <nhanhtt@liftsoft.vn>
2026-02-06 11:27:04 +0700

    update_tool

--- polls/key.py
+++ polls/key.py
@@ -4,26 +4,23 @@ import os
 import sys
 
 
-def badFunctionName(x, y, z, a, b, c, d, e, f, g):
-    result = x+y
-    if result > 10:
-        print("Big")
-    else:
-        if result < 0:
-            if result < -10:
-                if result < -20:
-                    print("Nested too deep")
-    return result
+def badFunctionName(x,y,z,a,b,c,d,e,f,g):  
+  result=x+y
+  if result>10: print("Big")   
+  else: 
+      if result<0: 
+          if result<-10: 
+              if result<-20: 
+                  print("Nested too deep")   
+  return result
 
-
-class myclass:
+class myclass:   
     def __init__(self):
         pass
 
-    def only_one_method(self):
+    def only_one_method(self):  
         print("demo")
 
-
 def unused_variable_demo():
-    a = 123
+    a = 123   
     return True
